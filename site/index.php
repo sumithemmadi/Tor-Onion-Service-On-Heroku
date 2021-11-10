6c4345c1ec964b8ac95cd373fce4a45ec9ac7524
Emmadi Sumith Kumar <sumithemmadi244@gmail.com>
2021-11-10 23:53:34 +0530

    Update index.php

--- site/index.php
+++ site/index.php
@@ -47,6 +47,291 @@
         <p> <b>Try my another  GitHub Repository <a href=https://github.com/sumithemmadi/tor-hidden-service-on-heroku>https://github.com/sumithemmadi/tor-hidden-service-on-heroku</a>
       </div>
     </section>
+<h2 id="-basic-usage-https-github-com-sumithemmadi-truecallerjs-"><a href="https://github.com/sumithemmadi/truecallerjs">Basic Usage</a></h2>
+<h3 id="installation">Installation</h3>
+<pre><code class="lang-bash">npm <span class="hljs-keyword">install</span> truecallerjs
+</code></pre>
+<h3 id="usage">Usage</h3>
+<pre><code class="lang-js"><span class="hljs-keyword">const</span> truecallerjs = <span class="hljs-built_in">require</span>(<span class="hljs-string">'truecallerjs'</span>);
+
+<span class="hljs-keyword">var</span> searchData = {
+                <span class="hljs-attr">number</span>: <span class="hljs-string">"[PHONE_NUMBER]"</span>,
+                <span class="hljs-attr">countryCode</span>: <span class="hljs-string">"[COUNTRY_CODE]"</span>,
+                <span class="hljs-attr">installationId</span>: <span class="hljs-string">"[INSTALLATION_ID]"</span>
+        }
+
+<span class="hljs-keyword">var</span> sn = truecallerjs.searchNumber(searchData);
+sn.then(<span class="hljs-function"><span class="hljs-keyword">function</span>(<span class="hljs-params">response</span>) </span>{
+    <span class="hljs-built_in">console</span>.log(response)
+});
+</code></pre>
+<p>If you want output in different format&#39;s.</p>
+<pre><code class="lang-js"><span class="hljs-keyword">const</span> truecallerjs = <span class="hljs-built_in">require</span>(<span class="hljs-string">'truecallerjs'</span>);
+
+<span class="hljs-keyword">var</span> searchData = {
+                <span class="hljs-attr">number</span>: <span class="hljs-string">"[PHONE_NUMBER]"</span>,
+                <span class="hljs-attr">countryCode</span>: <span class="hljs-string">"[COUNTRY_CODE]"</span>,
+                <span class="hljs-attr">installationId</span>: <span class="hljs-string">"[INSTALLATION_ID]"</span>,
+                <span class="hljs-attr">output</span>: <span class="hljs-string">"[FORMAT]"</span>
+        }
+
+<span class="hljs-keyword">var</span> sn = truecallerjs.searchNumber(searchData);
+sn.then(<span class="hljs-function"><span class="hljs-keyword">function</span>(<span class="hljs-params">response</span>) </span>{
+    <span class="hljs-built_in">console</span>.log(response)
+});
+</code></pre>
+<ul>
+<li><strong>PHONE_NUMBER</strong> : Number you want to search.</li>
+<li><strong>COUNTRY_CODE</strong> : Country code you want to use by default . If mobile number is not in <strong>E164</strong>(International Format) Format then <strong>COUNTRY_CODE</strong> will be considered as a country code of that Mobile Number.</li>
+<li><strong>FORMAT</strong> : Format of the output.<br>
+   1 . JSON <br>
+   2 . XML<br>
+   3 . YAML<br>
+   4 . TEXT<br>
+   5 . HTML</li>
+<li><p><strong>INSTALLATION_ID</strong> : To know your InstallationId , install the package globally.</p>
+</li>
+<li><p><strong>InstallationId</strong></p>
+<pre><code class="lang-bash">npm <span class="hljs-keyword">install</span> -g  truecallerjs
+</code></pre>
+</li>
+<li><a href="https://github.com/sumithemmadi/truecallerjs#Login">Login to you account</a>.</li>
+<li>Enter the below command to see your <strong>InstallationId</strong>.</li>
+</ul>
+<pre><code class="lang-bash">truecallerjs <span class="hljs-comment">--installationid</span>
+</code></pre>
+<h3 id="examples">Examples</h3>
+<ul>
+<li><strong>Example for JSON response.</strong>
+```js
+const truecallerjs = require(&#39;truecallerjs&#39;);</li>
+</ul>
+<p>var searchData = {
+                number: &quot;9912345678&quot;,
+                countryCode: &quot;IN&quot;,
+                installationId: &quot;a1k07--Vgdfyvv_rftf5uuudhuhnkljyvvtfftjuhbuijbhug&quot;,
+                output: &quot;JSON&quot;
+        }</p>
+<p>var sn = truecallerjs.searchNumber(searchData);
+sn.then(function(response) {
+    console.log(response)
+});</p>
+<pre><code>Here mobile number <span class="hljs-keyword">is</span> <span class="hljs-keyword">in</span> national format <span class="hljs-keyword">and</span> country code <span class="hljs-keyword">is</span> indian.
+So india <span class="hljs-keyword">is</span> considered as a country code <span class="hljs-keyword">of</span> that mobile number <span class="hljs-keyword">and</span> response will be json format.
+
+- **Example <span class="hljs-keyword">for</span> XML response.**
+`<span class="javascript"></span>``<span class="javascript">js
+<span class="hljs-keyword">const</span> truecallerjs = <span class="hljs-built_in">require</span>(<span class="hljs-string">'truecallerjs'</span>);
+
+<span class="hljs-keyword">var</span> searchData = {
+                <span class="hljs-attr">number</span>: <span class="hljs-string">"+12093031250"</span>,
+                <span class="hljs-attr">countryCode</span>: <span class="hljs-string">"IN"</span>,
+                <span class="hljs-attr">installationId</span>: <span class="hljs-string">"a1k07--Vgdfyvv_rftf5uuudhuhnkljyvvtfftjuhbuijbhug"</span>,
+                <span class="hljs-attr">output</span>: <span class="hljs-string">"XML"</span>
+        }
+
+<span class="hljs-keyword">var</span> sn = truecallerjs.searchNumber(searchData);
+sn.then(<span class="hljs-function"><span class="hljs-keyword">function</span>(<span class="hljs-params">response</span>) </span>{
+    <span class="hljs-built_in">console</span>.log(response)
+});</span>
+</code></pre><p>Here mobile number is in international format. So it is a US number.</p>
+<ul>
+<li><strong>Example for YAML response</strong>
+```js
+const truecallerjs = require(&#39;truecallerjs&#39;);</li>
+</ul>
+<p>var searchData = {
+                number: &quot;09912345678&quot;,
+                countryCode: &quot;IN&quot;,
+                installationId: &quot;a1k07--Vgdfyvv_rftf5uuudhuhnkljyvvtfftjuhbuijbhug&quot;,
+                output: &quot;YAML&quot;
+        }</p>
+<p>var sn = truecallerjs.searchNumber(searchData);
+sn.then(function(response) {
+    console.log(response)
+});</p>
+<pre><code>- **Example <span class="hljs-keyword">for</span> TEXT response**
+`<span class="javascript"></span>``<span class="javascript">js
+<span class="hljs-keyword">const</span> truecallerjs = <span class="hljs-built_in">require</span>(<span class="hljs-string">'truecallerjs'</span>);
+
+<span class="hljs-keyword">var</span> searchData = {
+                <span class="hljs-attr">number</span>: <span class="hljs-string">"09912345678"</span>,
+                <span class="hljs-attr">countryCode</span>: <span class="hljs-string">"IN"</span>,
+                <span class="hljs-attr">installationId</span>: <span class="hljs-string">"a1k07--Vgdfyvv_rftf5uuudhuhnkljyvvtfftjuhbuijbhug"</span>,
+                <span class="hljs-attr">output</span>: <span class="hljs-string">"TEXT"</span>
+        }
+
+<span class="hljs-keyword">var</span> sn = truecallerjs.searchNumber(searchData);
+sn.then(<span class="hljs-function"><span class="hljs-keyword">function</span>(<span class="hljs-params">response</span>) </span>{
+    <span class="hljs-built_in">console</span>.log(response)
+});</span>
+</code></pre><ul>
+<li><strong>Example for HTML Table</strong>
+```js
+const truecallerjs = require(&#39;truecallerjs&#39;);</li>
+</ul>
+<p>var searchData = {
+                number: &quot;09912345678&quot;,
+                countryCode: &quot;IN&quot;,
+                installationId: &quot;a1k07--Vgdfyvv_rftf5uuudhuhnkljyvvtfftjuhbuijbhug&quot;,
+                output: &quot;HTML&quot;
+        }</p>
+<p>var sn = truecallerjs.searchNumber(searchData);
+sn.then(function(response) {
+    console.log(response)
+});
+```</p>
+<p><strong>Table</strong></p>
+<table>
+   <tr>
+      <th>Properties</th>
+      <th>Values</th>
+   </tr>
+   <tr>
+      <td>data</td>
+      <td>
+   <tr>
+      <td>id</td>
+      <td>jsiebejebbeebhee/dnss00w==</td>
+   </tr>
+   <tr>
+      <td>name</td>
+      <td>Sumith Emmadi</td>
+   </tr>
+   <tr>
+      <td>imId</td>
+      <td>1g7rm006b356o</td>
+   </tr>
+   <tr>
+      <td>gender</td>
+      <td>UNKNOWN</td>
+   </tr>
+   <tr>
+      <td>image</td>
+      <td><a href="https://storage.googleapis.com/tc-images-noneu/myview/1/jdvdidbdhvdjdvddbkdbeiebeieb">https://storage.googleapis.com/tc-images-noneu/myview/1/jdvdidbdhvdjdvddbkdbeiebeieb</a></td>
+   </tr>
+   <tr>
+      <td>score</td>
+      <td>0.9</td>
+   </tr>
+   <tr>
+      <td>access</td>
+      <td>PUBLIC</td>
+   </tr>
+   <tr>
+      <td>enhanced</td>
+      <td>true</td>
+   </tr>
+   <tr>
+      <td>phones</td>
+      <td>
+   <tr>
+      <td>e164Format</td>
+      <td>+000000000000</td>
+   </tr>
+   <tr>
+      <td>numberType</td>
+      <td>MOBILE</td>
+   </tr>
+   <tr>
+      <td>nationalFormat</td>
+      <td>+000000000000</td>
+   </tr>
+   <tr>
+      <td>dialingCode</td>
+      <td>91</td>
+   </tr>
+   <tr>
+      <td>countryCode</td>
+      <td>IN</td>
+   </tr>
+   <tr>
+      <td>carrier</td>
+      <td>Vodafone Idea</td>
+   </tr>
+   <tr>
+      <td>type</td>
+      <td>openPhone</td>
+   </tr>
+   </td></tr>
+   <tr>
+      <td>addresses</td>
+      <td>
+   <tr>
+      <td>city</td>
+      <td>Andhra Pradesh</td>
+   </tr>
+   <tr>
+      <td>countryCode</td>
+      <td>IN</td>
+   </tr>
+   <tr>
+      <td>timeZone</td>
+      <td>+05:30</td>
+   </tr>
+   <tr>
+      <td>type</td>
+      <td>address</td>
+   </tr>
+   </td></tr>
+   <tr>
+      <td>internetAddresses</td>
+      <td>
+   <tr>
+      <td>id</td>
+      <td>email@gmail.com</td>
+   </tr>
+   <tr>
+      <td>service</td>
+      <td>email</td>
+   </tr>
+   <tr>
+      <td>caption</td>
+      <td>Sumith Emmadi</td>
+   </tr>
+   <tr>
+      <td>type</td>
+      <td>internetAddress</td>
+   </tr>
+   </td></tr>
+   <tr>
+      <td>badges</td>
+      <td>[verified][user]</td>
+   </tr>
+   <tr>
+      <td>cacheTtl</td>
+      <td></td>
+   </tr>
+   <tr>
+      <td>sources</td>
+      <td>[]</td>
+   </tr>
+   <tr>
+      <td>searchWarnings</td>
+      <td>[]</td>
+   </tr>
+   <tr>
+      <td>surveys</td>
+      <td>[]</td>
+   </tr>
+   </td></tr>
+   <tr>
+      <td>provider</td>
+      <td>ss-nu</td>
+   </tr>
+   <tr>
+      <td>stats</td>
+      <td>
+   <tr>
+      <td>sourceStats</td>
+      <td>[]</td>
+   </tr>
+   </td></tr>
+</table>
+
+<h2 id="license">License</h2>
+<p><a href="https://github.com/sumithemmadi/truecallerjs"><img src="https://app.fossa.com/api/projects/git%2Bgithub.com%2Fsumithemmadi%2Ftruecallerjs.svg?type=large" alt="FOSSA Status"></a></p>
+
     <footer>
       <p>Tor Onion Service On Heroku, Copyright &copy; 2021</p>
     </footer>
